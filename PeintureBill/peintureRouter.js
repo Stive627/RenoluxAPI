341aa32c52f69fcbdced0bb604797dcbe02c2bac
Stive627 <fossistive627@gmail.com>
2025-04-21 18:22:51 +0530

    first commit

--- PeintureBill/peintureRouter.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const { peintureDevis } = require('./peintureController')
-const peintureRouter = express.Router()
-peintureRouter.post('/', peintureDevis)
-
-module.exports = peintureRouter
--- PlacoBill/placoRouter.js
+++
@@ -1,5 +0,0 @@
-const express = require('express')
-const { placoDevis } = require('./placoController')
-const placoRouter = express.Router()
-placoRouter.post('/', placoDevis)
-module.exports = placoRouter 
